23c1bbc59682ccc26a6ebc54a2e530ac912e9a80
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2021-02-10 13:59:36 +0500

    A ship is added. The file 'game_functions' is added. The initial code was refactored.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
-
 import pygame
 
 from settings import Settings
+from ship import Ship
+import game_functions
 
 
 def run_game():
@@ -13,21 +13,14 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Set the background color.
-    bg_color = (230, 230, 230)
+    # Make a ship
+    ship = Ship(screen)
 
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        game_functions.check_events()
+        game_functions.update_screen(ai_settings, screen, ship)
 
 
 run_game()
